7ee600b3ad11f71a4330edfa3da54e4c46b802a0
dypso9 <51255157+dypso9@users.noreply.github.com>
2026-08-04 11:38:47 -0600

    Remove date filter from liquor sales query
    Removed date filter from SQL query in ETL script.

--- etl_iowa_liquor.py
+++ etl_iowa_liquor.py
@@ -42,7 +42,6 @@ def run_iowa_liquor_etl():
             volume_sold_liters,
             volume_sold_gallons
         FROM `bigquery-public-data.iowa_liquor_sales.sales`
-        WHERE date >= '2025-11-04' and date <= '2026-05-02'
         LIMIT 100
         -- Filtro opcional para limitar el tamaño de extracción
     """
